9720cc45f6c64ab72e1398d76251a4e0aa313bf7
Ruth Asmamaw <asmamawruth@gmail.com>
2024-07-11 14:39:10 +0300

    tab

--- src/app/admin/layoutComponent/tab.tsx
+++ src/app/admin/layoutComponent/tab.tsx
@@ -13,7 +13,7 @@ export default function Demo() {
     <Tabs defaultValue="gallery w-[1000px] h-auto bg-blue-200">
       <div className="flex flex-col gap-5 w-[100%]">
         <div className="flex w-[1000px] h-auto">
-          <Tabs.List className="flex gap-10 px-10 py-4 border border-green-500 rounded-[200px] w-full">
+          <Tabs.List className="flex gap-10 px-10 py-4 border border-green-500 rounded-[50px] w-full">
             <Tabs.Tab value="gallery">Update</Tabs.Tab>
             <Tabs.Tab value="messages">Add</Tabs.Tab>
             <Tabs.Tab value="settings">Delete</Tabs.Tab>
