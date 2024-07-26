d87966bcb83c0215ae33e7ed481a264d7ae06f60
Rahelrzel <rahelrzele@gmail.com>
2024-07-26 11:59:11 +0300

    the topic tree

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,28 +1,61 @@
 "use client";
 import { useRef, useState } from "react";
-import { Box, Button, Flex } from "@mantine/core";
+import { Box, Button } from "@mantine/core";
 import Navbar from "./component/Navbar";
-import Sidebar from "./component/updateSide_bar";
-import { Node } from "@/types/treeNode";
 import { MDXEditorMethods } from "@mdxeditor/editor";
 import { ForwardRefEditor } from "./component/mdxeditor/ForwardRefEditor";
-import Update from "./component/update";
 import { useRouter } from "next/navigation";
+import TopicsSideBar from "./component/components/TopicsSideBar";
+import { Topic } from "@/types/topic";
+
+// Sample data for topics and subtopics
+const data: Topic[] = [
+  { id: "1", name: "Getting Started", content: "", parentId: null },
+  { id: "2", name: "DataSource", content: "", parentId: null },
+  { id: "3", name: "Working with Data Source", content: "", parentId: "2" },
+  { id: "4", name: "Data Source Options", content: "", parentId: "2" },
+  { id: "5", name: "Multiple Data Sources", content: "", parentId: "2" },
+  { id: "6", name: "DataSource API", content: "", parentId: "2" },
+  { id: "7", name: "Entity", content: "", parentId: null },
+  { id: "8", name: "What is Entity?", content: "", parentId: "7" },
+  { id: "9", name: "Embedded Entities", content: "", parentId: "7" },
+  { id: "10", name: "Entity Inheritance", content: "", parentId: "7" },
+  { id: "11", name: "Tree Entities", content: "", parentId: "7" },
+  { id: "12", name: "View Entities", content: "", parentId: "7" },
+  {
+    id: "13",
+    name: "Separating Entity Definition",
+    content: "",
+    parentId: "7",
+  },
+  { id: "14", name: "Relations", content: "", parentId: null },
+  { id: "15", name: "What are Relations?", content: "", parentId: "14" },
+  { id: "16", name: "One-to-One", content: "", parentId: "14" },
+  {
+    id: "17",
+    name: "Many-to-One and One-to-Many",
+    content: "",
+    parentId: "14",
+  },
+  { id: "18", name: "Many-to-Many", content: "", parentId: "14" },
+  { id: "19", name: "Eager and Lazy Relations", content: "", parentId: "14" },
+  { id: "20", name: "Relations FAQ", content: "", parentId: "14" },
+];
 
 export default function Admin() {
   const router = useRouter();
   const [activeContent, setActiveContent] = useState<string>("");
   const ref = useRef<MDXEditorMethods>(null);
 
-  const handleNodeClick = (node: Node) => {
+  const handleNodeClick = (node: Topic) => {
     console.log(ref);
     if (ref.current) {
       const currentMarkdown = ref.current.getMarkdown();
       console.log("Current Markdown:", currentMarkdown);
-      ref.current.setMarkdown(`# ${node.title}`);
+      ref.current.setMarkdown(`# ${node.name}`);
     }
     setActiveContent(node.content || "");
-    const path = `/admin/subtitle/${node.title}`;
+    const path = `/admin/subtitle/${node.name}`;
     router.push(path);
   };
 
@@ -36,9 +69,7 @@ export default function Admin() {
         <Navbar />
       </Box>
       <Box className="flex flex-row h-full pt-16">
-        <Sidebar onNodeClick={handleNodeClick} />
-
-        {/* <Update /> */}
+        <TopicsSideBar topics={data} onNodeClick={handleNodeClick} />
         <Box className="editor flex-grow overflow-hidden relative">
           <Box className="editor-content h-full overflow-y-auto">
             <ForwardRefEditor
