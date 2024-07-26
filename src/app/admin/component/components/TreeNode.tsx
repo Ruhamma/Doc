909455963070d557c88a2f14d0faf7070d3aa229
Rahelrzel <rahelrzele@gmail.com>
2024-07-26 12:21:52 +0300

    Type error fixed

--- src/app/admin/component/components/TreeNode.tsx
+++ src/app/admin/component/components/TreeNode.tsx
@@ -3,23 +3,31 @@ import { Button } from "@mantine/core";
 import { Topic } from "@/types/topic";
 
 interface TreeNodeProps {
-  node: Topic & { subTopics: Topic[] };
-  onNodeClick: (node: Topic) => void;
+	node: Topic;
+	onNodeClick: (node: Topic) => void;
 }
 
 const TreeNode = ({ node, onNodeClick }: TreeNodeProps) => (
-  <div className="ml-4 mt-2">
-    <div>
-      <Button variant="subtle" color="green" onClick={() => onNodeClick(node)}>
-        {node.name}
-      </Button>
-    </div>
-    <div>
-      {node.subTopics.map((subTopic) => (
-        <TreeNode key={subTopic.id} node={subTopic} onNodeClick={onNodeClick} />
-      ))}
-    </div>
-  </div>
+	<div className="ml-4 mt-2">
+		<div>
+			<Button
+				variant="subtle"
+				color="green"
+				onClick={() => onNodeClick(node)}
+			>
+				{node.name}
+			</Button>
+		</div>
+		<div>
+			{node.subTopics?.map((subTopic) => (
+				<TreeNode
+					key={subTopic.id}
+					node={subTopic}
+					onNodeClick={onNodeClick}
+				/>
+			))}
+		</div>
+	</div>
 );
 
 export default TreeNode;
