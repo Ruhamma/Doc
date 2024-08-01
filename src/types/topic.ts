1cd6de4e217d0333697c1fa1459288d982d97ecd
Rahelrzel <rahelrzele@gmail.com>
2024-08-01 22:53:39 +0300

    subtopic

--- src/types/topic.ts
+++ src/types/topic.ts
@@ -1,33 +1,33 @@
 export interface NewTopicBody {
-	name: string;
+  name: string;
 }
 
 export interface NewSubtopicBody {
-	name: string;
-	parentCategoryId: string;
+  name: string;
+  parentCategoryId: string;
 }
 
 export interface UpdateDoc {
-	name: string;
-	content: string;
+  name: string;
+  content: string;
 }
 
 export interface SubTopic {
-	id: string;
-	name: string;
-	content: string;
-	url: string;
-	parentId: string | null;
-	createdAt: string;
-	updatedAt: string;
+  id: string;
+  name: string;
+  content: string;
+  url: string;
+  parentId: string | null;
+  createdAt: string;
+  updatedAt: string;
 }
 
 export interface Topic {
-	id: string;
-	name: string;
-	content: string;
-	parentId: string | null;
-	subTopics?: Topic[];
+  id: string;
+  name: string;
+  content: string;
+  parentId: string | null;
+  subTopics?: Topic[];
 }
 
 export type TopicsResponse = Topic[];
